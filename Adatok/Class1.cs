d03b5f5610c90d7f2b5a36b679d8b747086244c9
Krizsicskomark <krizsicskomark@ktch.hu>
2024-03-25 15:40:00 +0100

    Add project files.

---
+++ Adatok/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Bankkartya
+{
+    public string OwnerName { get; set; }
+    public string CardNumber { get; set; }
+    public DateTime ExperitationDate { get; set; }
+    public int Balance { get; set; }
+
+    public Bankkartya(string ownername, string cardnumber, DateTime experitationdate, int balance)
+    {
+        OwnerName = ownername;
+        CardNumber = cardnumber;
+        ExperitationDate = experitationdate;
+        Balance = balance;
+    }
+    public void PenzFelvetel(int osszeg)
+    {
+        int Balance = 0;
+        if (osszeg > 0 && osszeg <= Balance)
+        {
+            Balance -= osszeg;
+            Console.WriteLine($"Sikeres pénzfelvétel: {osszeg} Ft");
+        }
+        else
+        {
+            Console.WriteLine("Nem sikerült a pénzfelvétel. Hibás összeg vagy nincs elegendő egyenleg.");
+        }
+    }
+    public void KartyaAdataiKiiras()
+    {
+        Console.WriteLine($"Tulajdonos neve: {OwnerName}");
+        Console.WriteLine($"Kártyaszám: {CardNumber}");
+        Console.WriteLine($"Lejárati dátum: {ExperitationDate}");
+        Console.WriteLine($"Egyenleg: {Balance} Ft");
+    }
+}
---
+++ BankAdat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Bankkartya bankkartya = new Bankkartya("Kelemen Lajos", "3225636263", new DateTime(2025, 03, 12), 100000);
+        bankkartya.KartyaAdataiKiiras();
+        bankkartya.PenzFelvetel(5000);
+        bankkartya.KartyaAdataiKiiras();
+    }
+}
